3e11eb84040c3935b1cec762ea9f9e5ee60e5660
Kendrick Wong <kendrickw@gmail.com>
2024-09-13 18:05:24 -0400

    fix: cypress to test off production build

--- cypress/support/navigate.ts
+++ cypress/support/navigate.ts
@@ -1,9 +1,5 @@
 Cypress.Commands.add('navigatePropertyList', () => {
-  /**
-   * The initial visit to root may take a little longer if the server is a dev
-   * next server
-   */
-  cy.visit('/community', { timeout: 5000 });
+  cy.visit('/community');
   cy.clickMainMenu();
   cy.clickMenuItem('Select Community');
   cy.findByRole('option', { name: 'Test Community' }).click();
