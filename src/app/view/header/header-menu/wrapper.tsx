82204b513a12a52f5890026bfd7743ff8ea6de51
Kendrick Wong <kendrickw@gmail.com>
2025-01-27 09:23:16 -0500

    fix: repaint header menu when its size changes

--- src/app/view/header/header-menu/wrapper.tsx
+++ src/app/view/header/header-menu/wrapper.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import { useSession } from 'next-auth/react';
 import React from 'react';
-import { useWindowSize } from 'usehooks-ts';
+import { useResizeObserver, useWindowSize } from 'usehooks-ts';
 
 interface Props {
   className?: string;
@@ -12,8 +12,9 @@ export const HeaderMenuWrapper: React.FC<React.PropsWithChildren<Props>> = ({
   children,
 }) => {
   const { status } = useSession();
-  const { width } = useWindowSize();
+  const { width: windowWidth } = useWindowSize();
   const divRef = React.useRef<HTMLDivElement>(null);
+  const { width: menuWidth } = useResizeObserver({ ref: divRef });
   const [leftPos, setLeftPos] = React.useState<number>();
 
   React.useEffect(() => {
@@ -23,14 +24,15 @@ export const HeaderMenuWrapper: React.FC<React.PropsWithChildren<Props>> = ({
      * left postion
      */
     const elem = document.getElementById('nav-bar-avatar');
-    const divWidth = divRef.current?.offsetWidth;
-    if (elem && divWidth) {
+    if (elem && menuWidth) {
       const rect = elem.getBoundingClientRect();
-      setLeftPos(rect.right - divWidth);
+      setLeftPos(rect.right - menuWidth);
     }
   }, [
-    // we want to recalculate button position when screen size changes
-    width,
+    // we want to recalculate button position when screen size changes or
+    // when the header menu width changes
+    windowWidth,
+    menuWidth,
   ]);
 
   if (status !== 'authenticated') {
