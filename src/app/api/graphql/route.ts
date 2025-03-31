fe2915e68de129f6bc6dfbb6acea36c715893d9a
Kendrick Wong <kendrickw@gmail.com>
2025-03-31 13:04:30 -0400

    fix: security vulnerabilities (#146)

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,9 +1,12 @@
 import { usePersistedOperations } from '@graphql-yoga/plugin-persisted-operations';
-import { createYoga } from 'graphql-yoga';
+import { createYoga, useErrorHandler } from 'graphql-yoga';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { createContext } from '~/graphql/context';
 import persistedOperations from '~/graphql/generated/persisted-documents.json';
 import { schema } from '~/graphql/schema';
+import { Logger } from '~/lib/logger';
+
+const logger = Logger('graphql/route');
 
 /** Allow only persisted graphQL queries */
 // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -12,6 +15,13 @@ const persistedQueryPlugin = usePersistedOperations({
     // @ts-expect-error: imported JSON should have Record type
     return persistedOperations[sha256Hash];
   },
+});
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const errorHandlerPlugin = useErrorHandler(({ errors, context, phase }) => {
+  errors.forEach((error) => {
+    logger.error(error);
+  });
 });
 
 const yoga = createYoga<{
@@ -30,7 +40,7 @@ const yoga = createYoga<{
     Response: Response,
     Request: Request,
   },
-  plugins: [persistedQueryPlugin],
+  plugins: [errorHandlerPlugin, persistedQueryPlugin],
   /** See https://the-guild.dev/graphql/yoga-server/docs/features/error-masking */
   // maskedErrors: false,
 });
