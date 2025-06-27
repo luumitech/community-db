8fd519c14c0cd60ae85e406ed62953611df19446
Kendrick Wong <kendrickw@gmail.com>
2025-06-27 10:03:01 -0400

    fix: perform env var schema validation once only in runtime (#192)

--- src/app/lib/env-cfg.ts
+++ src/app/lib/env-cfg.ts
@@ -2,6 +2,7 @@ import { jsonc } from 'jsonc';
 import { unstable_noStore } from 'next/cache';
 import { Logger } from '~/lib/logger';
 import { z, zz } from '~/lib/zod';
+import { isBuilding } from './env-var';
 
 const logger = Logger('env-cfg');
 
@@ -27,7 +28,7 @@ const baseSchema = z.object({
    *
    * @example `protocol://hostname:port`
    */
-  NEXT_PUBLIC_HOSTNAME: zz.string.nonEmpty(),
+  NEXT_PUBLIC_HOSTNAME: zz.string.envVar(),
   /** App version information (used in about modal) */
   NEXT_PUBLIC_APP_VERSION: z.string().optional(),
   NEXT_PUBLIC_GIT_BRANCH: z.string().optional(),
@@ -36,12 +37,12 @@ const baseSchema = z.object({
   /** Is Subscription plan enabled? */
   NEXT_PUBLIC_PLAN_ENABLE: zz.coerce.toBoolean(),
   /** Name and cost of Free plan */
-  NEXT_PUBLIC_PLAN_FREE_NAME: zz.string.nonEmpty(),
+  NEXT_PUBLIC_PLAN_FREE_NAME: zz.string.envVar(),
   NEXT_PUBLIC_PLAN_FREE_COST: z.coerce.number(),
   NEXT_PUBLIC_PLAN_FREE_MAX_COMMUNITY: z.coerce.number(),
   NEXT_PUBLIC_PLAN_FREE_MAX_PROPERTY: z.coerce.number(),
   /** Name and cost of Premium plan */
-  NEXT_PUBLIC_PLAN_PREMIUM_NAME: zz.string.nonEmpty(),
+  NEXT_PUBLIC_PLAN_PREMIUM_NAME: zz.string.envVar(),
   NEXT_PUBLIC_PLAN_PREMIUM_COST: z.coerce.number(),
   NEXT_PUBLIC_PLAN_PREMIUM_MAX_COMMUNITY: z.coerce.number(),
   NEXT_PUBLIC_PLAN_PREMIUM_MAX_PROPERTY: z.coerce.number(),
@@ -55,55 +56,55 @@ const baseSchema = z.object({
   CONFIG_DEBUG: zz.coerce.toBoolean(),
 
   /** Cypher keys */
-  CIPHER_KEY: zz.string.nonEmpty(),
-  CIPHER_IV: zz.string.nonEmpty(),
+  CIPHER_KEY: zz.string.envVar(),
+  CIPHER_IV: zz.string.envVar(),
 
   /** Google related env vars */
   /** For google login */
-  GOOGLE_CLIENT_ID: zz.string.nonEmpty(),
-  GOOGLE_CLIENT_SECRET: zz.string.nonEmpty(),
+  GOOGLE_CLIENT_ID: zz.string.envVar(),
+  GOOGLE_CLIENT_SECRET: zz.string.envVar(),
 
   /** Mongo configurations */
-  MONGODB_URI: zz.string.nonEmpty(),
+  MONGODB_URI: zz.string.envVar(),
 
   /** Uploadthing configuration */
-  UPLOADTHING_TOKEN: zz.string.nonEmpty(),
+  UPLOADTHING_TOKEN: zz.string.envVar(),
 
   /** Geoapify */
-  GEOAPIFY_URL: zz.string.nonEmpty(),
-  GEOAPIFY_KEY: zz.string.nonEmpty(),
+  GEOAPIFY_URL: zz.string.envVar(),
+  GEOAPIFY_KEY: zz.string.envVar(),
 
   /** Payment Configuration */
   PAYMENT_HELCIM_PLAN_ID: z.coerce.number(),
-  PAYMENT_HELCIM_API_KEY: zz.string.nonEmpty(),
+  PAYMENT_HELCIM_API_KEY: zz.string.envVar(),
 
   /** Google Recaptcha configuration */
   NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY: z.string(),
   GOOGLE_RECAPTCHA_SECRET_KEY: z.string(),
 
   /** Nodemailer configuration */
-  EMAIL_SERVER_USER: zz.string.nonEmpty(),
-  EMAIL_SERVER_PASSWORD: zz.string.nonEmpty(),
-  EMAIL_SERVER_HOST: zz.string.nonEmpty(),
+  EMAIL_SERVER_USER: zz.string.envVar(),
+  EMAIL_SERVER_PASSWORD: zz.string.envVar(),
+  EMAIL_SERVER_HOST: zz.string.envVar(),
   EMAIL_SERVER_PORT: z.coerce.number(),
   EMAIL_SERVER_SECURE: zz.coerce.toBoolean(),
-  EMAIL_FROM: zz.string.nonEmpty(),
+  EMAIL_FROM: zz.string.envVar(),
 });
 
 const azureStorageSchema = z.discriminatedUnion('AZURE_STORAGE_MODE', [
   z.object({
     /** Local Azurite configuration */
     AZURE_STORAGE_MODE: z.literal('local'),
-    AZURE_LOCAL_STORAGE_HOST: zz.string.nonEmpty(),
+    AZURE_LOCAL_STORAGE_HOST: zz.string.envVar(),
     AZURE_LOCAL_STORAGE_PORT: z.coerce.number(),
-    AZURE_LOCAL_STORAGE_ACCOUNT: zz.string.nonEmpty(),
-    AZURE_LOCAL_STORAGE_ACCOUNT_KEY: zz.string.nonEmpty(),
+    AZURE_LOCAL_STORAGE_ACCOUNT: zz.string.envVar(),
+    AZURE_LOCAL_STORAGE_ACCOUNT_KEY: zz.string.envVar(),
   }),
   z.object({
     /** Remote Azure storage configuration */
     AZURE_STORAGE_MODE: z.literal('remote'),
-    AZURE_STORAGE_ACCOUNT: zz.string.nonEmpty(),
-    AZURE_STORAGE_ACCOUNT_KEY: zz.string.nonEmpty(),
+    AZURE_STORAGE_ACCOUNT: zz.string.envVar(),
+    AZURE_STORAGE_ACCOUNT_KEY: zz.string.envVar(),
   }),
   z.object({
     AZURE_STORAGE_MODE: z.literal('none'),
@@ -111,23 +112,31 @@ const azureStorageSchema = z.discriminatedUnion('AZURE_STORAGE_MODE', [
 ]);
 
 const schema = baseSchema.and(azureStorageSchema);
+let _env: z.infer<typeof schema> | null = null;
 
 /**
  * Validate process.env against zod schema and returned typed version
  *
  * @returns Typed version of process.env
  */
-export function initializeEnv() {
+function initializeEnv() {
   // Prevent NextJs from caching this function
   unstable_noStore();
 
-  const verifiedEnv = schema.parse(process.env);
-
-  if (verifiedEnv.CONFIG_DEBUG) {
-    logger.info(jsonc.stringify(verifiedEnv, undefined, 2));
+  if (isBuilding()) {
+    return schema.parse(process.env);
   }
 
-  return verifiedEnv;
+  // During runtime, we only want to verify env var against schema once
+  if (_env == null) {
+    _env = schema.parse(process.env);
+
+    if (_env.CONFIG_DEBUG) {
+      logger.info(jsonc.stringify(_env, undefined, 2));
+    }
+  }
+
+  return _env;
 }
 
 export const env = initializeEnv();
--- src/app/lib/zod/string.ts
+++ src/app/lib/zod/string.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { isBuilding, isRunningTest } from '~/lib/env-var';
 
 export class String {
   private zStr(msg?: string) {
@@ -13,6 +14,28 @@ export class String {
    */
   nonEmpty(_msg?: string) {
     const msg = _msg ?? 'Must specify a value';
+
     return this.zStr(msg).min(1, msg);
+  }
+
+  /**
+   * This should only be used in `env-cfg.ts`, or environment variable schema
+   * validation. In addition to checking for nonEmpty string, it also checks
+   *
+   * - '...' is not allowed
+   */
+  envVar() {
+    const msg = 'Environment variable expects a string';
+
+    return this.nonEmpty(msg).refine((str) => {
+      return isBuilding() || isRunningTest()
+        ? /**
+           * During build or test time, the `.env` file contains placeholder string
+           * "...", so this allows schema validation to not flag them. During runtime,
+           * they should be replaced with actual value.
+           */
+          true
+        : str !== '...';
+    }, msg);
   }
 }
