a49a6ba36e9286ea0bec7de698df00f296de1d11
Kendrick Wong <kendrickw@gmail.com>
2025-12-18 10:26:09 -0500

    fix: remove @graphql-tools/executor-http

--- src/app/graphql/test-util/graphql-util.ts
+++ src/app/graphql/test-util/graphql-util.ts
@@ -1,20 +1,22 @@
-import {
-  buildHTTPExecutor,
-  HTTPExecutorOptions,
-} from '@graphql-tools/executor-http';
-import { AsyncExecutor, ExecutionRequest } from '@graphql-tools/utils';
+import type { TypedDocumentNode } from '@graphql-typed-document-node/core';
+import { print, type ExecutionResult } from 'graphql';
 import { createYoga } from 'graphql-yoga';
 import type { Context } from '~/graphql/context';
 import { pubSub } from '~/graphql/pubsub';
 import { schema } from '~/graphql/schema';
 import { type ContextUser } from '~/lib/context-user';
 
+interface ExecuteSingle<TResult, TVariables> {
+  document: TypedDocumentNode<TResult, TVariables>;
+  variables?: TVariables;
+}
+
 /** Helper test utility class for invoking graphQL query/mutation */
 export class GraphQLUtil {
-  private _executor: AsyncExecutor<unknown, HTTPExecutorOptions>;
+  #yoga = createYoga({ schema });
 
   // Default context for graphQL
-  private _context: Context = {
+  #context: Context = {
     user: {
       email: process.env.AUTH_TEST_EMAIL!,
     },
@@ -22,46 +24,35 @@ export class GraphQLUtil {
     clientIp: '127.0.0.1',
   };
 
-  constructor() {
-    const yoga = createYoga({ schema });
-    this._executor = buildHTTPExecutor({ fetch: yoga.fetch });
-  }
-
   /** Convenience method to return apollo context */
   get context(): Context {
-    return this._context;
+    return this.#context;
   }
 
   /** Set the authorized user in the context */
   setAuthUser(user: ContextUser) {
-    this._context = {
-      ...this._context,
-      user,
-    };
+    this.#context = { ...this.#context, user };
   }
 
-  /** Execute the graphql query and expect the result to be non iteratable */
-  async executeSingle<
-    TVariables extends Record<string, unknown> = Record<string, unknown>,
-    TRootValue = unknown,
-    TReturn = unknown,
-  >(
-    request: ExecutionRequest<
-      TVariables,
-      Context,
-      TRootValue,
-      HTTPExecutorOptions,
-      TReturn
-    >
-  ) {
-    const result = await this._executor({
-      context: this._context,
-      ...request,
-    });
-    if (Symbol.asyncIterator in result) {
-      throw new Error('Expected single value');
-    }
-
-    return result;
+  /** Execute the graphql query */
+  async executeSingle<TResult, TVariables>(
+    arg: ExecuteSingle<TResult, TVariables>
+  ): Promise<ExecutionResult<TResult>> {
+    const result = await this.#yoga.fetch(
+      'http://yoga/graphql',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({
+          query: print(arg.document),
+          variables: arg.variables,
+        }),
+      },
+      this.#context
+    );
+    return result.json();
   }
 }
