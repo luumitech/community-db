56289c1d5bf8509d37840e92b6d1848b773cbe49
Kendrick Wong <kendrickw@gmail.com>
2026-04-10 14:46:19 -0400

    fix: mongo pooling problems (#327)

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,23 +1,18 @@
 /** This is for server use only */
 import { PrismaClient } from '@prisma/client';
-import { isProduction } from '~/lib/env';
 
-let prisma: PrismaClient;
+/**
+ * Prisma will be attached to the global object so that you do not exhaust the
+ * database connection limit. For more details, check out the documentation for
+ * Next.js and Prisma Client best practices:
+ *
+ * https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
+ */
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-if (isProduction()) {
-  prisma = new PrismaClient();
-} else {
-  /**
-   * Prisma will be attached to the global object so that you do not exhaust the
-   * database connection limit. For more details, check out the documentation
-   * for Next.js and Prisma Client best practices:
-   *
-   * https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
-   */
-  // @ts-expect-error: don't want to introduce prisma to global type
-  global.prisma ??= new PrismaClient();
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+globalForPrisma.prisma ??= prisma;
 
-  // @ts-expect-error: don't want to introduce prisma to global type
-  prisma = global.prisma;
-}
 export default prisma;
