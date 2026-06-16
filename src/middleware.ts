1b27c69bf22c25fa44fbae398d537f78bbdf7b82
Kendrick Wong <kendrickw@gmail.com>
2026-06-16 15:05:04 -0400

    feat: update to next@16 (#352)

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { authMiddleware } from './middlewares/auth';
-import { chain } from './middlewares/chain';
-
-/** Middle wares are executed in order until it returns a response */
-export default chain([
-  // Verify authentication
-  authMiddleware,
-]);
